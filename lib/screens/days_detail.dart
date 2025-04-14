0dba65cfa608d3dba40281f319d68be35e72b434
victor.telles@iteso.mx <victor.telles@iteso.mx>
2025-04-14 08:53:10 -0600

    Nuevo apartado para el perfil detalles

---
+++ lib/screens/days_detail.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class DaysDetail extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("Tus Dias")),
+      body: Center(child: Text("Aquí aparecerán tus dias")),
+    );
+  }
+}
---
+++ lib/screens/exercises_detail.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class ExercisesDetail extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("Tus Ejercicios")),
+      body: Center(child: Text("Aquí aparecerán tus Ejercicios")),
+    );
+  }
+}
